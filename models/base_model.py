79f51efa195e418579ee684411ce16a499502578
AAEmara <abdelrahmanemara18@gmail.com>
2023-12-04 13:46:51 -0800

    AirBnB_clone-Emara: Task4 BaseModel Update.

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ from datetime import datetime
 class BaseModel():
     """A class used to define all common attr./methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializing the instance attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs):
+            for key, val in kwargs.items():
+                if key == "__class__":
+                    pass
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(val))
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the formatted output of the print statement on an instance.
