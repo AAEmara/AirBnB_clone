ac63523a7e87db00022f961dc00291c0a1c7333e
AAEmara <abdelrahmanemara18@gmail.com>
2023-12-04 13:49:22 -0800

    AirBnB_clone-Emara: BaseModel Unit test Update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
     """Testing the BaseModel Class."""
     def test_attr(self):
@@ -15,23 +16,37 @@ class TestBaseModel(unittest.TestCase):
     def test_save(self):
         """Testing save method."""
         inst_2 = BaseModel()
-        self.assertEqual(inst_2.created_at, inst_2.updated_at)
+        self.assertNotEqual(inst_2.created_at, inst_2.updated_at)
         inst_2.save()
         self.assertNotEqual(inst_2.created_at, inst_2.updated_at)
 
     def test_to_dict(self):
-        """Tasting to_dict method."""
+        """Testing to_dict method."""
         inst_3 = BaseModel()
         created_at_bef = inst_3.created_at
         updated_at_bef = inst_3.updated_at
-        keys = list(inst_3.__dict__)
-        self.assertNotIn("__class__", keys)
+        inst_3_attr = list(inst_3.__dict__)
+        self.assertNotIn("__class__", inst_3_attr)
         dict_return = inst_3.to_dict()
         created_at_aft = inst_3.created_at
         updated_at_aft = inst_3.updated_at
         keys_update = list(inst_3.__dict__)
         self.assertIn("__class__", keys_update)
-        self.assertNotEqual(created_at_bef, created_at_aft)
-        self.assertNotEqual(updated_at_bef, updated_at_aft)
+        self.assertEqual(created_at_bef,
+                         datetime.fromisoformat(created_at_aft))
+        self.assertEqual(updated_at_bef,
+                         datetime.fromisoformat(updated_at_aft))
         self.assertEqual(type(created_at_aft), str)
         self.assertEqual(type(updated_at_aft), str)
+
+    def test_kwargs(self):
+        """Testing the usage of kwargs and instance recreation."""
+        inst_4 = BaseModel()
+        kwargs_dict = inst_4.to_dict()
+        inst_4_attr = list(kwargs_dict)
+        self.assertIn("__class__", inst_4_attr)
+        inst_5 = BaseModel(kwargs_dict)
+        inst_5_attr = list(inst_5.__dict__)
+        self.assertNotIn("__class__", inst_5_attr)
+        self.assertEqual(type(inst_5.created_at), datetime)
+        self.assertEqual(type(inst_5.updated_at), datetime)
