e8c67b07fe7073077c914545cba14379f2adcd24
AAEmara <abdelrahmanemara18@gmail.com>
2023-12-06 16:15:11 -0800

    AirBnB_clone-Emara: BaseModel Update.

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Module that defines the BaseModel class."""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
@@ -21,6 +22,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Returns the formatted output of the print statement on an instance.
@@ -30,6 +32,7 @@ class BaseModel():
     def save(self):
         """Updates the updated_at instance attribute with current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of an instance.
@@ -43,6 +46,10 @@ class BaseModel():
             A dictionary containing all the key/values of an instance.
         """
         self.__dict__["__class__"] = str(BaseModel.__name__)
+        if isinstance(self.created_at, str):
+            self.created_at = datetime.fromisoformat(self.created_at)
         self.created_at = self.created_at.isoformat()
+        if isinstance(self.updated_at, str):
+            self.updated_at = datetime.fromisoformat(self.updated_at)
         self.updated_at = self.updated_at.isoformat()
         return (self.__dict__)
